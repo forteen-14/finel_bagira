fe2f2e97e9850622a436ec1958d4d69a48cdf42f
forteen14 <nh-shalevm@ortarad.ort.org.il>
2023-08-22 10:25:47 +0300

    update all

---
+++ game_field.py
@@ -0,0 +1,69 @@
+import consts
+import random
+
+
+def create_characters(field):
+
+    #make the soldier take 2X4 spots in the top left and the flag 3X4 in the bottom right
+    for row in range(4):
+        for col in range(2):
+            field[row][col] = consts.SOLDIER
+    for row in range(4):
+        for col in range(3):
+            field[row][consts.NUM_OF_COLS - col] = consts.FLAG
+    #put 20 random grass spots
+    for i in range(consts.GRASS_AMOUNT):
+        #check if the spot is already taken ny soldier or flag
+        while True:
+            row = random.randint(0, consts.NUM_OF_ROWS - 1)
+            col = random.randint(0, consts.NUM_OF_COLS - 1)
+            if field[row][col] != consts.SOLDIER and field[row][col] != consts.FLAG:
+                field[row][col] = consts.GRASS
+                break
+
+
+def create_mines(field):
+    #put 20 random mines
+    for i in range(consts.MINES_AMOUNT):
+        try:
+        #check if the spot is already taken ny soldier or flag
+            while True:
+                #mines take 3 spots in a row
+                row = random.randint(0, consts.NUM_OF_ROWS - 1)
+                col = random.randint(0, consts.NUM_OF_COLS - 3)
+                if field[row][col] != consts.SOLDIER and field[row][col] != consts.FLAG and field[row][col + 1] != consts.SOLDIER and field[row][col + 1] != consts.FLAG and field[row][col + 2] != consts.SOLDIER and field[row][col + 2] != consts.FLAG:
+                    field[row][col] = consts.MINE
+                    field[row][col + 1] = consts.MINE
+                    field[row][col + 2] = consts.MINE
+                    break
+        except:
+            pass
+
+
+def create_field():
+    # create the field 2d list
+    game_filed = [[consts.GRASS for col in range(consts.NUM_OF_COLS)] for row in range(consts.NUM_OF_ROWS)]
+    create_characters(game_filed)
+    create_mines(game_filed)
+
+
+    return game_filed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,10 +1,57 @@
+import pygame
+import consts
+import game_field
+import screen
+import soldier
 
+state = {
+    "State": consts.RUNNING_STATE,
+    "is_window_open": True,
+}
+
+
+
+def event_handler():
+    while state["is_window_open"]:
+        # Cycles through all the events currently occuring
+        for event in pygame.event.get():
+
+            # Condition becomes true when keyboard is pressed
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    pass
+                if event.key == pygame.K_LEFT:
+                    pass
+                if event.key == pygame.K_UP:
+                    pass
+                if event.key == pygame.K_DOWN:
+                    pass
+                if event.key == pygame.K_ESCAPE:
+                    state["is_window_open"] = False
 
 
 def main():
-
-
+    # initialize pygame screen
+    pygame.init()
+    screen = pygame.display.set_mode((900, 600))
+    pygame.display.set_caption(consts.GAME_NAME)
+    screen.fill(consts.DARK_GREEN)
+    pygame.display.flip()
+    field = game_field.create_field()
+    event_handler()
+    state["is_window_open"]
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
