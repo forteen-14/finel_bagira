fd57a6a1665ef6845e7e7da0df481ee54b13600f
h <dan086421@gmail.com>
2023-08-22 15:22:41 +0300

    x

--- screen.py
+++ screen.py
@@ -1,6 +1,6 @@
 import consts
 import pygame
-
+import time
 screen = pygame.display.set_mode(
     (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 
@@ -70,7 +70,8 @@ def draw_game(state,field):
         draw_image(name,info,field)
     if state["state"]==consts.LOSE_STATE:
         draw_lose_message()
+        time.sleep(3)
     elif state["state"]==consts.WIN_STATE:
         draw_win_message()
-
+        time.sleep(3)
     pygame.display.flip()
